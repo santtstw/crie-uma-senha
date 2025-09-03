fbf23a72b3741304dcad237adad16d343545297f
santtstw <vitor.ciombalo.santos@escola.pr.gov.br>
2025-09-03 14:04:47 -0300

    asdfghj

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ const botoes = document.querySelectorAll('.parametro-senha-botao');
 const forcaSenha = querySelector('.forca');
 botoes[0].onclick = diminuiTamanho
 botoes[1].onclick = aumentaTamanho
+function diminuiTamanho(){
+    if (tamanhoSenha >1){
+        tamanhoSenha --;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function diminuiTamanho(){
+    if (tamanhoSenha <20){
+        tamanhoSenha ++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+const camposSenha = document.querySelector('#campo-senha');
