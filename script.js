813dff56a91e827791f1bec7a2f343815481b033
santtstw <vitor.ciombalo.santos@escola.pr.gov.br>
2025-09-03 14:26:46 -0300

    asdfg

--- script.js
+++ script.js
@@ -21,3 +21,37 @@ function diminuiTamanho(){
     geraSenha();
 }
 const camposSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = ' 0123456789';
+const simbolos = '!@%*?';
+function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+     if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+     if (checkbox[2].checked){
+        alfabeto = alfabeto + letrasNumeros;
+    }
+     if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i ++){
+        let numeroAleatorio = Math.random ()* alfabeto.length;
+        numeroaleatorio = Math.floor ( numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+    classificaSenha(alfabeto.length);
+}
+function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    forcaSenha.classList.remove('fraca','media','forte')
+    if(entropia >57){
+        
+    }
+}
